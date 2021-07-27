5b1fb4bd3953392366876aa2f17c077393106ca9
Elisha Olade <elishaolade@gmail.com>
2021-07-27 10:37:50 -0700

    Added changes to index page and also still working on saving user

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,21 @@
 import Head from 'next/head'
-
+import React, {useEffect} from 'react';
+import Login from '../components/UI/Login/Login';
+import { useStateContext } from '../components/HBOProvider';
+import router from 'next/router';
 
 export default function Home() {
+  const globalState = useStateContext();
+  useEffect(() => {
+    const loggedIn = false;
+    if(!loggedIn) {
+      // Sends users to create page
+      router.push('/create');
+    }
+  }, [])
   return (
     <div>
-      <div className="login-user">
-        <div className="login-user__top">
-          <div className="login-user__logo"/>
-          <span className="login-user__title">
-            Who Is Watching?
-          </span>
-        </div>
-
-        <div className="login-user__form">
-          <div className="login-user__user-box">
-            <img className="login-user__user-img" src="https://uifaces.co/our-content/donated/vIqzOHXj.jpg" />
-            <div className="login-user__user-name">Bryant</div>
-          </div>
-        </div>
-        <div className="login-user__buttons">
-          <button className="login-user__adult">Add Adult</button>
-          <button className="login-user__kid">Add Kid</button>
-        </div>
-      </div>
+      <Login />
     </div>
   )
 }
